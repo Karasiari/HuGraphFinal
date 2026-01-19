9f5bf2f5656fc9a6949e62cfc2c213f0459cc81c
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-01-19 19:47:23 +0300

    Create instruments_for_exps.py

---
+++ graph_exps/instruments_for_exps.py
@@ -0,0 +1,19 @@
+from .core import HuGraphForExps
+
+# вспомогательные функции для основного экспа - для расчетов, параллелизаций и проч
+
+def compute_alpha_for_edge(graph_state, source, target):
+    # Восстанавливаем граф из сериализованного состояния (например, pickle)
+    import pickle
+    graph = pickle.loads(graph_state)
+    
+    # Берём первый мультребро
+    keys = list(graph.multigraph.get_edge_data(source, target).keys())
+    if not keys:
+        return ((source, target), float('nan'))
+    key = keys[0]
+    
+    graph.change_multiedge(source, target, "insert", key, 80)
+    alpha = graph.calculate_alpha()
+    # graph.restore_graph() не нужен, т.к. граф временный
+    return ((source, target), alpha)
