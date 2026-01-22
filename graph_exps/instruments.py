3ebbdbc49f8798eaeafc8db492b1929dc6dcf1c6
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-01-22 18:59:06 +0300

    Update instruments.py

--- graph_exps/instruments.py
+++ graph_exps/instruments.py
@@ -12,8 +12,7 @@ from scipy.linalg import fractional_matrix_power
 # -------------------------------------------------------------------------------------
 
 class Demand:
-    def __init__(self, id: int, source: int, sink: int, capacity: float):
-        self.id = id
+    def __init__(self, source: int, sink: int, capacity: float):
         self.source = source
         self.sink = sink
         self.capacity = capacity
