977ae333a6f3f8266e7c228487b4f78fefc2e697
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-01-22 20:09:51 +0300

    Create main_algorithm.py

---
+++ graph_exps/mcf_algorithm/main_algorithm.py
@@ -0,0 +1,39 @@
+from .instruments import *
+
+# -----------------------------------------------------------------------------------------------------------------------
+# основная функция для решения MCF (проложенные запросы, индексы проложенных запросов, флаг - проложились ли все запросы)
+# -----------------------------------------------------------------------------------------------------------------------
+
+def solve_mcf_problem(G_copy, C_max, demands, unsatisfied_subset, eps):
+  # Step 1: Group demands and create the mapping from i to source-target pairs
+  grouped_demands, demand_indices_by_group, i_to_source_target = group_demands_and_create_mapping(demands,
+                                                                                                  unsatisfied_subset)
+        
+  # Step 2: Run the multicommodity flow procedure to generate the flow and l(e) values
+  flow = multi_commodity_flow(G_copy, grouped_demands, C_max, eps)
+
+  # Step 3: Scale the flow to make it feasible (ensures flows respect edge capacities)
+  scale_flows(flow, G_copy, C_max)
+
+  # Step 4: Subdivide flows by paths for ungrouped demands
+  flow_paths, satisfied_demands = subdivide_flows_by_paths(flow, demand_indices_by_group, demands,
+                                                           i_to_source_target)
+
+  # Step 5: Subtract the satisfied demands from the graph capacity
+  graph_copy = subtract_flow_from_capacity(G_copy, flow_paths, demands)
+
+  satisfied_demands_set = set(satisfied_demands)
+  left_to_satisfy = unsatisfied_subset - satisfied_demands_set
+        
+  # Step 6: Try to fulfill remaining demands in the leftover graph
+  remaining_paths, remaining_satisfied_demands = fulfill_remaining_demands(graph_copy, demands,
+                                                                           demand_indices_by_group,
+                                                                           i_to_source_target, left_to_satisfy)
+
+  # Combine the satisfied demands
+  satisfied_demands += remaining_satisfied_demands
+  satisfied_demands_dict = {id: (demands[id].source, demands[id].target, demands[id].capacity) for id in satisfied_demands}
+  flow_paths.update(remaining_paths)
+  solved = unsatisfied_subset == set(satisfied_demands)
+
+  return flow_paths, satisfied_demands_dict, solved
