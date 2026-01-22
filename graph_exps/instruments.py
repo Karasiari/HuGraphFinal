5b6ccc18002e1b69957b1a6660d03de828fe4e05
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-01-22 20:26:19 +0300

    Update instruments.py

--- graph_exps/instruments.py
+++ graph_exps/instruments.py
@@ -130,27 +130,3 @@ def get_pinv_sqrt(laplacian: np.ndarray) -> np.ndarray:
     L_pinv = np.linalg.pinv(laplacian)
     L_pinv_sqrt = fractional_matrix_power(L_pinv, 0.5)
     return L_pinv_sqrt
-
-# ------------------------------------------------------------------
-# вспомогательные функции для MCFP (Maximum Concurrent Flow Problem)
-# ------------------------------------------------------------------
-
-def get_incidence_matrix_for_mcfp(graph: nx.DiGraph) -> np.ndarray:
-    """
-    Создаем incidence matrix
-    параметры: nx.DiGraph
-    return: incidence_matrix
-    """
-    incidence_matrix = nx.incidence_matrix(graph, edgelist=graph.edges, oriented=True)
-    incidence_matrix = incidence_matrix.toarray()
-    return incidence_matrix
-
-def get_capacities_for_mcfp(graph: nx.DiGraph) -> np.ndarray:
-    """
-    Достаем capacities
-    параметры: nx.DiGraph, граф с capacities (key: weights) на ребрах
-    return: np.ndarray
-    """
-    edges_with_weights = [(edge, data['weight']) for edge, data in graph.edges.items()]
-    edges_with_weights_dict = {key: value for key, value in edges_with_weights}
-    return np.array(list(edges_with_weights_dict.values()), dtype=np.float64)
