dadb00e44946fa6e4e25f3ab2423885b2fc79f44
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-01-24 16:40:56 +0300

    Update classes_for_algorithm.py

--- graph_exps/spare_capacity_allocation_algorithm/classes_for_algorithm.py
+++ graph_exps/spare_capacity_allocation_algorithm/classes_for_algorithm.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 from itertools import pairwise
 from typing import Any, Callable, Dict, FrozenSet, Hashable, List, Mapping, NewType, Optional, Sequence, Tuple
 import random
-import copy
 
 import networkx as nx
 
