a077f67f34ba097b6f0ba332018f6e8fe2d38eca
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-01-25 17:15:25 +0300

    Update output_converter.py

--- graph_exps/spare_capacity_allocation_algorithm/output_converter.py
+++ graph_exps/spare_capacity_allocation_algorithm/output_converter.py
@@ -13,16 +13,16 @@ from .classes_for_algorithm import (
 )
 
 def convert_greedy_output_for_exp(SpareCapacityGreedyOutput) -> Tuple[Dict[Tuple[int, int], Tuple[nx.Graph, nx.Graph]], int, float]:
-  remaining_network_dict: Dict[Tuple[int, int], Tuple[nx.Graph, nx.Graph]] = {}
-  
-  for edge_key, remaining_network in SpareCapacityGreedyOutput.remaining_network_by_failed_edge.items():
-    remaining_network_dict[(edge_key[0], edge_key[1])] = remaining_network
+    remaining_network_dict: Dict[Tuple[int, int], Tuple[nx.Graph, nx.Graph]] = {}
     
-  if SpareCapacityGreedyOutput.algorithm_failure_flag:
-      algorithm_failure_flag = 1
-  else:
-      algorithm_failure_flag = 0
-  
-   successfully_rerouted_demands_ratio = SpareCapacityGreedyOutput.successfully_rerouted_demands_ratio
+    for edge_key, remaining_network in SpareCapacityGreedyOutput.remaining_network_by_failed_edge.items():
+        remaining_network_dict[(edge_key[0], edge_key[1])] = remaining_network
+    
+    if SpareCapacityGreedyOutput.algorithm_failure_flag:
+        algorithm_failure_flag = 1
+    else:
+        algorithm_failure_flag = 0
+      
+     successfully_rerouted_demands_ratio = SpareCapacityGreedyOutput.successfully_rerouted_demands_ratio
    
-   return (remaining_network_dict, algorithm_failure_flag, successfully_rerouted_demands_ratio)
+     return (remaining_network_dict, algorithm_failure_flag, successfully_rerouted_demands_ratio)
