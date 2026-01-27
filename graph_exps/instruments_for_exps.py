6e9d189fba940bb1d07d6158aab454d99ba7ea31
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-01-27 18:35:00 +0300

    Update instruments_for_exps.py

--- graph_exps/instruments_for_exps.py
+++ graph_exps/instruments_for_exps.py
@@ -37,21 +37,10 @@ def expand_graph(graph: HuGraphForExps, source_target_sequence_to_add: List[Tupl
 
 # функция для решения перераспределения трафика - в решении наш алгоритм
 
-def allocate_spare_capacity(graph_version: Tuple[str, HuGraphForExps], random_seed: int | None = None) -> Tuple[str, Tuple[Dict[Tuple[int, int], Tuple[nx.Graph, nx.Graph]], int, float]]:
-    allocation_type, graph = graph_version
+def allocate_spare_capacity(graph: HuGraphForExps, allocation_type: str, random_seed: int | None = None) -> Tuple[str, Tuple[Dict[Tuple[int, int], Tuple[nx.Graph, nx.Graph]], int, float]]:
     route_result, demands, solved = graph.solve_mcf()
     input_for_algorithm = convert_to_greedy_input(graph.multigraph, demands, route_result, random_seed)
     output_of_algorithm = run_greedy_spare_capacity_allocation(input_for_algorithm)
     allocation_results = convert_greedy_output_for_exp(output_of_algorithm)
 
     return (allocation_type, allocation_results)
-
-def allocate_spare_capacity_i(graph_state, random_seed: int | None = None) -> Tuple[str, Tuple[Dict[Tuple[int, int], Tuple[nx.Graph, nx.Graph]], int, float]]:
-    import pickle
-    graph = pickle.loads(graph_state)
-    route_result, demands, solved = graph.solve_mcf()
-    input_for_algorithm = convert_to_greedy_input(graph.multigraph, demands, route_result, random_seed)
-    output_of_algorithm = run_greedy_spare_capacity_allocation(input_for_algorithm)
-    allocation_results = convert_greedy_output_for_exp(output_of_algorithm)
-
-    return ('alpha', allocation_results)
