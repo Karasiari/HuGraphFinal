775e8e9cce3199e8aa7b71bc086558582cdb1eb5
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-01-29 15:30:20 +0300

    Update main_algorithm.py

--- graph_exps/mcfp_algorithm/main_algorithm.py
+++ graph_exps/mcfp_algorithm/main_algorithm.py
@@ -8,7 +8,7 @@ from .instruments import *
 # основной алгоритм
 # -----------------
 
-def solve_max_concurrent_flow_problem(graph: nx.DiGraph, demands_laplacian: np.ndarray, break_flag: bool = True, solver_flag: bool) -> float | None:
+def solve_max_concurrent_flow_problem(graph: nx.DiGraph, demands_laplacian: np.ndarray, solver_flag: bool, break_flag: bool = True) -> float | None:
   # получаем incidence matrix и capacities рёбер
   incidence_mat = get_incidence_matrix_for_mcfp(graph)
   bandwidth = get_capacities_for_mcfp(graph)
