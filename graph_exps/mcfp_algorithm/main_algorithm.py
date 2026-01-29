e7ac047cecf1addbf13879e5346b1e21ec39eeca
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-01-29 15:23:39 +0300

    Update main_algorithm.py

--- graph_exps/mcfp_algorithm/main_algorithm.py
+++ graph_exps/mcfp_algorithm/main_algorithm.py
@@ -8,7 +8,7 @@ from .instruments import *
 # основной алгоритм
 # -----------------
 
-def solve_max_concurrent_flow_problem(graph: nx.DiGraph, demands_laplacian: np.ndarray, solver_flag: bool) -> float:
+def solve_max_concurrent_flow_problem(graph: nx.DiGraph, demands_laplacian: np.ndarray, break_flag: bool = True, solver_flag: bool) -> float | None:
   # получаем incidence matrix и capacities рёбер
   incidence_mat = get_incidence_matrix_for_mcfp(graph)
   bandwidth = get_capacities_for_mcfp(graph)
@@ -27,47 +27,75 @@ def solve_max_concurrent_flow_problem(graph: nx.DiGraph, demands_laplacian: np.n
           gamma >= 0,
       ]
   )
-
+  
   # решаем задачу
   max_for_tries = 5
-
+  
   solver_error = False
   if not solver_flag:
-      try:
-          prob.solve(solver='CLARABEL')
-      except SolverError:
-          solver_error = True
-          prob.solve(solver='ECOS')
+    try:
+      prob.solve(solver='CLARABEL')
+    except SolverError:
+      solver_error = True
+      prob.solve(solver='ECOS')
   else:
       prob.solve(solver='CLARABEL')
   gamma = gamma.value if gamma is not None else None
   max_gamma = gamma
   current_try = 1
   if prob.status != "optimal":
-      gamma = None
-  while gamma is None and (current_try <= 5 or max_gamma is None):
-      if not solver_flag:
-          if not solver_error:
-              try:
-                  prob.solve(solver='CLARABEL')
-              except SolverError:
-                  solver_error = True
-                  current_try = 1
-                  max_gamma, gamma = None, None
-                  prob.solve(solver='ECOS')
-          else:
-              prob.solve(solver='ECOS')
-      else:
-          prob.solve(solver='CLARABEL')
-      if max_gamma is not None and gamma is not None:
-          max_gamma = max(max_gamma, gamma.value)
-      elif max_gamma is None and gamma is not None:
-          max_gamma = gamma.value
-      current_try += 1
-      if prob.status != "optimal":
-          gamma = None
-      gamma = gamma.value if gamma is not None else None
-
-  gamma = gamma if gamma is not None else max_gamma
+    gamma = None
   
-  return float(gamma)
+  if break_flag:
+    while gamma is None or (current_try <= 5):
+        if not solver_flag:
+            if not solver_error:
+                try:
+                    prob.solve(solver='CLARABEL')
+                except SolverError:
+                    solver_error = True
+                    current_try = 1
+                    max_gamma, gamma = None, None
+                    prob.solve(solver='ECOS')
+            else:
+                prob.solve(solver='ECOS')
+        else:
+            prob.solve(solver='CLARABEL')
+        if max_gamma is not None and gamma is not None:
+            max_gamma = max(max_gamma, gamma.value)
+        elif max_gamma is None and gamma is not None:
+            max_gamma = gamma.value
+        current_try += 1
+        if prob.status != "optimal":
+            gamma = None
+        gamma = gamma.value if gamma is not None else None
+
+    gamma = gamma if gamma is not None else max_gamma
+  else:
+    while gamma is None and (current_try <= 5 or max_gamma is None):
+        if not solver_flag:
+            if not solver_error:
+                try:
+                    prob.solve(solver='CLARABEL')
+                except SolverError:
+                    solver_error = True
+                    current_try = 1
+                    max_gamma, gamma = None, None
+                    prob.solve(solver='ECOS')
+            else:
+                prob.solve(solver='ECOS')
+        else:
+            prob.solve(solver='CLARABEL')
+        if max_gamma is not None and gamma is not None:
+            max_gamma = max(max_gamma, gamma.value)
+        elif max_gamma is None and gamma is not None:
+            max_gamma = gamma.value
+        current_try += 1
+        if prob.status != "optimal":
+            gamma = None
+        gamma = gamma.value if gamma is not None else None
+
+    gamma = gamma if gamma is not None else max_gamma
+
+  gamma = float(gamma) if gamma is not None else None
+  return gamma
