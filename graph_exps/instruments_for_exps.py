9f48ebe9e25cb19faacf3d1ca0011a1baafbc990
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-01-30 17:56:55 +0300

    Update instruments_for_exps.py

--- graph_exps/instruments_for_exps.py
+++ graph_exps/instruments_for_exps.py
@@ -4,6 +4,7 @@ import networkx as nx
 
 from .core import HuGraphForExps
 
+from .spare_capacity_allocation_alforithm.classes_for_algorithm import SpareCapacityGreedyInput
 from .spare_capacity_allocation_algorithm.input_converter import convert_to_greedy_input # импорт функции для преобразования данных под алгоритм перераспределения трафика
 from .spare_capacity_allocation_algorithm.main_algorithm import run_greedy_spare_capacity_allocation # импорт основной функции алгоритма перепрокладки
 from .spare_capacity_allocation_algorithm.output_converter import convert_greedy_output_for_exp # импорт функции для преобразования результата алгоритма перепрокладки под наш эксперимент
@@ -11,7 +12,7 @@ from .spare_capacity_allocation_algorithm.output_converter import convert_greedy
 # ----------------------------------------------------------------------------------
 # вспомогательные функции для основного экспа - для расчетов, параллелизаций и проч.
 # ----------------------------------------------------------------------------------
-
+ 
 def compute_alpha_for_edge(graph_state, source, target):
     # Восстанавливаем граф из сериализованного состояния (например, pickle)
     import pickle
@@ -35,15 +36,81 @@ def expand_graph(graph: HuGraphForExps, source_target_sequence_to_add: List[Tupl
     return graph
 
 
+# функция для отдельного расчета остаточных сетей
+
+def get_remaining_networks(graph: nx.MultiDiGraph, route_result: Dict[int, List[Tuple[int, int, int]]], demands: Dict[int, Tuple[int, int, int]]) -> Dict[Tuple[int, int], Tuple[nx.DiGraph, nx.MultiDiGraph]]:
+    slack_by_edge: Dict[Tuple[int, int], int] = {}
+    demands_through_edge: Dict[Tuple[int, int], List[int, ...]] = {}
+    unique_edges: Dict[Tuple[int, int], bool] = []
+    remaining_networks: Dict[Tuple[int, int], Tuple[nx.DiGraph, nx.MultiDiGraph]] = {}
+
+    for u, v, data in graph.edges(data=True):
+        edge_oriented = (u, v)
+        edge_unoriented = min(u, v), max(u, v)
+        if not unique_edges.get(edge_unoriented, False):
+            unique_edges[edge_unoriented] = True
+        if slack_by_edge.get(edge_oriented, False):
+            slack_by_edge[edge_oriented] += int(data['capacity'])
+        else:
+            slack_by_edge[edge_oriented] = int(data['capacity'])
+    for demand_id, demand_path in route_result.items():
+        demand = demands[demand_id]
+        demand_volume = demand[2]
+        for u, v, _ in demand_path:
+            edge_oriented = (u, v)
+            slack_by_edge[edge_oriented] -= demand_volume
+            if demands_through_edge.get(edge_oriented, False):
+                demands_through_edge[edge_oriented].append(demand_id)
+            else:
+                demands_through_edge[edge_oriented] = [demand_id]
+
+    for edge_unoriented, _ in unique_edges.items():
+        slack_graph = nx.DiGraph()
+        slack_demands_graph = nx.MultiDiGraph()
+        slack_graph.add_nodes_from(graph)
+        slack_demands_graph.add_nodes_from(graph)
+        edge_reversed = edge_unoriented[1], edge_unoriented[0]
+        affected_demands_ids = demands_through_edge[edge_unoriented] + demands_through_edge[edge_reversed]
+        affected_demands = []
+        edges = slack_by_edge.copy()
+        edges_list = []
+        for demand_id in affected_demands_ids:
+            affected_demand = demands[demand_id]
+            source, target, capacity = affected_demand
+            affected_demands.append((source, target, {"weight": capacity}))
+            edges_to_restore = route_result[demand_id]
+            for u, v, _ in edges_to_restore:
+                edge_oriented = (u, v)
+                edges[edge_oriented] += capacity
+        for edge_oriented, capacity in edges.items():
+            if edge_oriented not in (edge_unoriented, edge_reversed):
+                edges_list.append(edge_oriented, {"capacity": capacity})
+        slack_graph.add_edges_from(edges_list)
+        slack_demands_graph.add_edges_from(affected_demands)
+        remaining_networks[edge_unoriented] = (slack_graph, slack_demands_graph)
+    
+    return remaining_networks
+    
+
+# отдельная функция для предварительного решения MCF в рамках эксперимента
+
+def solve_mcf_for_exp(graph: HuGraphForExp, allocation_type: str) -> Tuple[SpareCapacityGreedyInput, Dict[Tuple[int, int], Tuple[nx.DiGraph, nx.MultiDiGraph]]]:
+    route_result, demands, solved, multidigraph = graph.solve_mcf()
+    remaining_networks = get_remaining_networks(multidigraph, route_result, demands)
+    input_for_allocate_spare_capacity_algorithm = convert_to_greedy_input(multidigraph, demands, route_result, random_seed)
+    return input_for_allocate_spare_capacity_algorithm, remaining_networks
+
+
 # функция для решения перераспределения трафика - в решении наш алгоритм
 
 def allocate_spare_capacity(graph: HuGraphForExps, allocation_type: str, random_seed: int | None = None) -> Tuple[str, Tuple[Dict[Tuple[int, int], Tuple[nx.Graph, nx.Graph]], int, float]]:
-    route_result, demands, solved = graph.solve_mcf()
-    input_for_algorithm = convert_to_greedy_input(graph.multigraph, demands, route_result, random_seed)
+    route_result, demands, solved, multigraph = graph.solve_mcf()
+    remaining_networks = get_remaining_networks(multigraph, route_result, demands)
+    input_for_algorithm = convert_to_greedy_input(multigraph, demands, route_result, random_seed)
     output_of_algorithm = run_greedy_spare_capacity_allocation(input_for_algorithm)
     allocation_results = convert_greedy_output_for_exp(output_of_algorithm)
 
-    return (allocation_type, allocation_results)
+    return (allocation_type, allocation_results, remaining_networks)
 
 
 # функция для решения max concurrent flow на остаточной сети (gamma) для параллельного расчета в рамках основного эксперимента
