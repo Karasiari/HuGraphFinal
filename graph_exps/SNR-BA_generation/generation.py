d00d341aabc341a446d3a4d23a9f3c5cb60b687a
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-02-02 17:56:33 +0300

    Update generation.py

--- graph_exps/SNR-BA_generation/generation.py
+++ graph_exps/SNR-BA_generation/generation.py
@@ -8,6 +8,15 @@ def get_weighted_graph(
     unweighted_graph: nx.Graph,
     capacity_value: float
 ) -> nx.MultiGraph:
+    """
+    Взвешивает сгенерированный SNR-BA граф
+    Input:
+          unweighted_graph - сгенерированный SNR-BA граф
+          capacity_value - значение веса для ребер
+     Output:
+          Граф с весом capacity_value для любого ребра 
+          как nx.MultiGraph, но без параллельных ребер
+    """
     weighted_graph: nx.MultiGraph = nx.MultiGraph()
     if capacity_value <= 0:
         raise ValueError("Значение capacity должно быть положительным")
@@ -17,13 +26,15 @@ def get_weighted_graph(
         weighted_graph.add_edge(u, v, {"capacity": capacity_value})
     return weighted_graph
 
+
 # основная функция генерации SNR-BA графа
 
 def generate_snr_ba_graph(
     num_nodes: int, 
     capacity_value: float, 
     random_seed: Optional[int] = None
-) -> nx.Graph():
+) -> nx.Graph:
     coords = generate_uniform_nodes(num_nodes, random_seed)
     snr_ba_graph_unweighted = snr_ba_from_latlon(coords, m=2, theta=5.0, seed=random_seed)
+    snr_ba_graph = get_weighted_graph(snr_ba_graph_unweighted, capacity_value)
     return snr_ba_graph
