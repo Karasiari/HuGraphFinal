a28e4c655ee483cf75b5603aa2c7ba05a721e5ee
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-02-02 19:36:56 +0300

    Update example.py

--- graph_exps/example.py
+++ graph_exps/example.py
@@ -1,3 +1,5 @@
+# импорт читалки ху графов
+from .hugraph_reader import read_hu_graphs
 # импорт генерации своего трафика
 from .traffic_generation.generation_for_exp import generate_own_traffic
 # импорт генераци SNR-BA графа
