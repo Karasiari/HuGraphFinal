4a85defa661702991842910040128bb0fb6d37ca
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-02-10 15:24:13 +0300

    Update output_converter.py

--- graph_exps/spare_capacity_allocation_algorithm/output_converter.py
+++ graph_exps/spare_capacity_allocation_algorithm/output_converter.py
@@ -6,9 +6,11 @@ from .classes_for_algorithm import (
     SpareCapacityGreedyOutput
 )
 
-def convert_greedy_output_for_exp(SpareCapacityGreedyOutput) -> Tuple[bool, float]:
+AddVolumeByEdge = Dict[Tuple[int, int], int]
+
+def convert_greedy_output_for_exp(SpareCapacityGreedyOutput) -> Tuple[bool, float, AddVolumeByEdge]:
     algorithm_solved_flag = not SpareCapacityGreedyOutput.algorithm_failure_flag
     successfully_rerouted_demands_volume = SpareCapacityGreedyOutput.successfully_rerouted_demands_volume
     additional_volume_by_edge = SpareCapacityGreedyOutput.additional_volume_by_edge
     
-    return (algorithm_solved_flag, successfully_rerouted_demands_volume)
+    return (algorithm_solved_flag, successfully_rerouted_demands_volume, additional_volume_by_edge)
