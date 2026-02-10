509b44c67606f2da794bfcef15aa884d05cd8b1f
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-02-10 22:12:12 +0300

    Update exp.py

--- graph_exps/exp.py
+++ graph_exps/exp.py
@@ -102,18 +102,8 @@ def expand_network_for_type(
         source_target_sequence_for_new_resources = list(zip(edges_to_expand, resources_to_add))
 
     # пробный тип
-    elif allocation_type == "random_mid_alpha":
-        mid_edges = filter_edges(edges_with_alphas, 0.8, 0.0)
-        random.shuffle(mid_edges)
-        edges_to_expand = [edge for edge, _ in mid_edges[:number_of_new_resources]]
-        source_target_sequence_for_new_resources = list(zip(edges_to_expand, resources_to_add)) 
-    elif allocation_type == "random_mid_mid_alpha":
+    elif allocation_type == "alpha_mid_random":
         mid_edges = filter_edges(edges_with_alphas, 0.7, 0.2)
-        random.shuffle(mid_edges)
-        edges_to_expand = [edge for edge, _ in mid_edges[:number_of_new_resources]]
-        source_target_sequence_for_new_resources = list(zip(edges_to_expand, resources_to_add)) 
-    elif allocation_type == "random_mid_mid_mid_alpha":
-        mid_edges = filter_edges(edges_with_alphas, 0.7, 0.1)
         random.shuffle(mid_edges)
         edges_to_expand = [edge for edge, _ in mid_edges[:number_of_new_resources]]
         source_target_sequence_for_new_resources = list(zip(edges_to_expand, resources_to_add)) 
