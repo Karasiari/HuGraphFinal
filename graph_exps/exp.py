f0cbe042fdd0519dc7edea65115cf4f9775b3345
Karasiari <124217577+Karasiari@users.noreply.github.com>
2026-02-13 18:46:38 +0300

    Update exp.py

--- graph_exps/exp.py
+++ graph_exps/exp.py
@@ -154,8 +154,10 @@ def expand_network_for_type(
         random.shuffle(mid_edges)
         edges_to_expand = [edge for edge, _ in mid_edges[:number_of_new_resources]]
         source_target_sequence_for_new_resources = list(zip(edges_to_expand, resources_to_add)) 
-    elif allocation_type == "alpha_with_support";
-        continue
+    elif allocation_type == "alpha_with_support":
+        support_edges_dict = {}
+        for edge, unexpanded_remaining_network in unexpanded_remaining_networks.items():
+          support_edges_dict[edge] = get_support_edge(unexpanded_remaining_network)
     else:
         raise ValueError(f"Тип распределения ресурсов {allocation_type} не предусмотрен экспериментом")
 
